ac77f8257a64fc7da1cdc342246d6665a77016e5
OpenShift System User <568e8dfb7628e1f8ee0000bc@ex-std-node614.prod.rhcloud.com>
2016-01-08 05:14:23 -0500

    maj mysql connection

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 
-echo phpinfo();
+$servername = "568e8eb50c1e661ec50001a7-cuibo.rhcloud.com";
+$username = "adminjZcePWq";
+$password = "gG95_u8qtSsn";
+$dbname = "php";
+$port = 56031;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+// Check connection
+if ($conn->connect_error) {
+	echo json_encode(array('return' => "KO" ));
+    die("Connection failed: " . $conn->connect_error);
+} 
+//echo json_encode(array('return' => "OK" ));
+
+$sql = "SELECT * FROM restos";
+$result = $conn->query($sql);
+$tabReturn = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $tabTmp = array(
+          'id' => $row['id'],
+          'resto' => $row['resto'],
+          'address' => $row['address'],
+          'latitude' => $row['latitude'],
+          'longitude' => $row['longitude'],
+          'zoom' => $row['zoom'],
+          'description' => $row['description']
+        );
+        array_push($tabReturn, $tabTmp);
+    }
+
+    echo json_encode($tabReturn);
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+
+
